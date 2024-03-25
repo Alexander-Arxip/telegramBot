546ed92ee85c9cc079dad5c5a75f5abf71ca8cd0
Aleksandr_S <durdom82@gmail.com>
2024-03-25 23:14:44 +0300

    Initial commit 01

---
+++ telegramBot.py
@@ -0,0 +1,116 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+
+
+# функция которая рисует доску 3х3.
+def draw_board(board):
+    for i in range(3):
+        print(' | '.join(board[i]))
+        print('-------')
+
+      # функция которая позволяет пользователю делать ход.
+def ask_and_make_move(player, board):
+    x,y = ask_move(player, board)
+    make_move(player, board, x, y )
+    '''
+    x,y = input(f"{player}, enter x and y coordinati (e.g. 0 0): " ).strip().split()
+    x,y = int(x), int(y)
+    if (0 <= x <=2) and (0<= y <= 2) and (board[x][y] ==  " "):
+        board[x][y] = player
+    else:
+        print('Eto mesto zanato. Poprobyiye eshe raz.')
+        ask_and_make_move(player,board)
+    '''
+
+      # функция позволяет пользователю ввести координаты хода
+def ask_move(player, board):
+    x,y = input(f"{player}, enter x and y coordinati (e.g. 0 0): " ).strip().split()
+    x, y = int(x), int(y)
+    if (0 <= x <= 2) and (0 <= y <= 2) and (board[x][y] == " "):
+        return ( x,y )
+    else:
+        print('Kletka zanata. Vvedite koordinatu esho raz.')
+        return ask_move(player, board)
+
+       # функция позволяет пользователю делать ход
+def make_move(player, board, x,y):
+    if board[x] [y] != "  ":
+        print('Izvenite. Kletka zanata')
+        return False
+    board[x] [y] = player
+    return True
+
+   # функция проверяющая стал ли ход выйгрышным.
+
+def check_win(player, board):
+    for i in range(3):
+        if board[i] == [player, player, player ]:
+            return True
+        if board[0][i] == player and board[1][i] == player and board[2][i] == player:
+            return True
+    if board [0] [0] == player and board[1][1] == player and board[2][2] == player:
+        return True
+    if board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        return  True
+    return False
+
+     # функция которая управляет ходом игры.
+
+def tic_toe():
+    while True:                  # цикл (Пока) -> Тру, значит -> бесконечный цикл игры
+        board = [ [ ' ' for i in range(3)] for j in range (3) ]
+        player = 'X'
+        while True:
+            draw_board(board)
+            ask_and_make_move(player, board)
+            if check_win(player, board):    # проверка выйграли ли игрок
+                print( f"{player} you Victory!" )
+                break
+            tie_game = False                # проверка на вариант -- ничья
+            for row in board:
+                for cell in row:
+                    if cell == " ":
+                        tie_game = True
+            if not tie_game:
+                break
+            player = '0' if player == 'X' else 'X'
+        resrart = input('Hotite eshe raz sigrat ???  (y/n) ')
+        if resrart.lower() != 'y':
+            break
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
